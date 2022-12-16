ee4579259a25dc9b9e7cb5e9d77ff5274ba743d6
Baptiste Pattyn <48830942+baptistepattyn@users.noreply.github.com>
2022-12-16 08:32:17 +0100

    Update ppt and cleanup script

--- cleandocker.ps1
+++ cleandocker.ps1
@@ -1,3 +1,10 @@
 ﻿docker container stop $(docker container ls -aq)
 docker container prune -f
 docker rmi $(docker images -q) -f
+$networks = docker network ls --format "{{.Name}}"
+foreach($network in $networks){
+    if(($network -ne "bridge") -and ($network -ne "host") -and ($network -ne "none")){
+        Write-Output "Removing" $network
+        docker network rm $network
+    }    
+}
